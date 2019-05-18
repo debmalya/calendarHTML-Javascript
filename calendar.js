6a037e71ebcfa04ebcce8dd402125c864f410980
Debmalya Jash <debmalya.jash@gmail.com>
2019-05-18 14:41:21 +0800

    Create calendar.js

---
+++ calendar.js
@@ -0,0 +1,116 @@
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let selectYear = document.getElementById("year");
+let selectMonth = document.getElementById("month");
+let appointments = {};
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+let monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+
+function next() {
+    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+    showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+    currentYear = parseInt(selectYear.value);
+    currentMonth = parseInt(selectMonth.value);
+    showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+
+    let firstDay = (new Date(year, month)).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+
+    let tbl = document.getElementById("calendar-body"); // body of the calendar
+
+    // clearing all previous cells
+    tbl.innerHTML = "";
+
+    // filing data about month and in the page via DOM.
+    monthAndYear.innerHTML = months[month] + " " + year;
+    selectYear.value = year;
+    selectMonth.value = month;
+
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        // creates a table row
+        let row = document.createElement("tr");
+
+        //creating individual cells, filing them up with data.
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            } else if (date > daysInMonth) {
+                break;
+            } else {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode(date);
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                    cell.classList.add("bg-info");
+                } // color today's date
+
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+
+                date++;
+            }
+
+
+        }
+
+        tbl.appendChild(row); // appending each row into calendar body.
+    }
+
+}
+
+
+
+$("#calendar").on("click", "td", function () {
+    let selectedDate = $(this).text();
+
+    if (selectedDate != "") {
+        let formattedDate = new Date(currentYear + "-" + (currentMonth + 1) + "-" + selectedDate);
+        if (new Date() <= formattedDate) {
+
+            if (appointments[formattedDate] != undefined) {
+                bookedHours = [];
+                for (var key in appointments[formattedDate]) {
+                    bookedHours.push(key);
+                }
+                alert("Already has booking for these hours : " + JSON.stringify(appointments[formattedDate], null, 4));
+            } else {
+                appointments[formattedDate] = {};
+            }
+
+            let event = prompt("Please enter event name", "Sample Event");
+            if (event) {
+                let start = parseInt(prompt("Please ente starting hour[00 - 23]?", "09"));
+                let end = parseInt(prompt("Please ente starting hour[00 - 23]?", start + 1));
+                let i = start;
+                while (i <= end) {
+                    appointments[formattedDate][i] = event;
+                    i++;
+                }
+            }
+
+
+        }
+    }
+});
